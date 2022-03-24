fbf3cf86c76526795664446a9ae4213d663e55d2
Alexandre Catro <62666344+AlehCastro@users.noreply.github.com>
2022-03-24 16:35:07 -0300

    Add files via upload

---
+++ Loja Impérios TEC (compras)/js/functions.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.conteiner .slides-list').slick({
+		centerMode:false,
+		slidesToShow:1,
+		arrows:false,
+		infinite:true,
+		dots:true,
+		autoplay: true,
+      	mobileFirst: true,
+     	respondTo: 'min',
+      	adaptiveHeight: true,
+	});
+}) 
+
